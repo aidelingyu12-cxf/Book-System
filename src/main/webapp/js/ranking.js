48916343e067dfa493912ced9ae6449c7c525bc3
aidelingyu12-cxf <aidelingyu12@gmail.com>
2024-08-29 21:31:19 +0800

    2024/8/29 modify ranking pages&&js

--- src/main/webapp/js/ranking.js
+++ src/main/webapp/js/ranking.js
@@ -14,6 +14,7 @@ function loadData() {
     //
     getHeaderList();
     //左側本のカタログを取得する
+    getRanking();
 
     var loadingMask = document.getElementById('loadingDiv');
     loadingMask.parentNode.removeChild(loadingMask);
@@ -23,7 +24,47 @@ function loadData() {
 
 }
 
-function hidefooter(){
+function getRanking() {
+  var xhttp = new XMLHttpRequest();
+  var xhttpResp = null;
+  var ul = document.getElementsByClassName("header-navi-ul")[0];
+  xhttp.open("GET", "http://localhost:3000/rankingList", false);
+  xhttp.send();
+  if (xhttp.status == 200 && xhttp.readyState == 4) {
+
+    for (var i = 1; i <= 10; i++) {
+      var xhttpResp = JSON.parse(xhttp.responseText)[0];
+      console.log(xhttpResp);
+      var ul = document.getElementsByClassName("ranking-detail")[0];
+      var li = document.createElement("li");
+      var a = document.createElement("a");
+      var spanLevel = document.createElement("span");
+      spanLevel.classList.add("level");
+      var spanTitle = document.createElement("span");
+      spanTitle.classList.add("title");
+      var spanDescribe = document.createElement("span");
+      spanDescribe.classList.add("describe");
+
+      //
+      spanLevel.innerText = xhttpResp["ranking"];
+      spanTitle.innerText = xhttpResp["title"];
+      spanDescribe.innerText = xhttpResp["detail"];
+      a.setAttribute("href", xhttpResp["url"]);
+
+      //
+      a.appendChild(spanLevel);
+      a.appendChild(spanTitle);
+      a.appendChild(spanDescribe);
+      li.appendChild(a);
+      ul.appendChild(li);
+    }
+
+    
+
+  }
+}
+
+function hidefooter() {
   var pagination = document.getElementsByClassName(".pagination")[0];
   pagination.style.display = "";
 }
